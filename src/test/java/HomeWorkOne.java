5cb8053c6060228959bb3c307905cbfeee870a55
AK <andrejs.kolesnikovs.lv@gmail.com>
2019-08-27 13:04:03 +0300

    adding files

---
+++ src/test/java/HomeWorkOne.java
@@ -0,0 +1,10 @@
+public class HomeWorkOne {
+    public double sum (Integer a, Integer b)
+    {return a * 100 / b; }
+    public void showSum () {
+        double c;
+        c = sum(30,40);
+    System.out.println (" C = " + c);
+
+    }
+}
